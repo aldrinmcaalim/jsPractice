72de9f0f0136fc6512b4cb24f40c4eeb73bcd9f1
Aldrin <aldrinmcaalim@gmail.com>
2021-03-24 00:03:19 -0400

    Practicing more

--- app.js
+++ app.js
@@ -7,15 +7,22 @@ const jonas = [
     true
 ];
 
-// for (let i = jonas.length - 1; i >= 0; i--) {
-//     console.log(i, jonas[i]);
+// console.log(`For Loop:`);
+// for (let rep = 1; rep < 11; rep++) {
+//     console.log(`Rep ${rep}`);
 // }
 
-for (let exercise = 1; exercise < 4; exercise++) {
-    console.log(`-------- Starting Exercise ${exercise}`);
-    for (let rep = 1; rep < 6; rep++) {
-        console.log(`Exercise ${exercise}: Lifting weights, rep ${rep}`);
-    }
-}
+// console.log(`While Loop:`);
+// let rep = 1;
+// while (rep < 11) {
+//     console.log(`Rep ${rep}`);
+//     rep++;
+// }
 
-console.log("finished");
+let die = Math.floor(Math.random() * 6) + 1;
+// console.log(die);
+while (die !== 6) {
+    console.log(`You rolled a ${die}!`);
+    die = Math.floor(Math.random() * 6) + 1;
+    if (die === 6) console.log("The loop has ended");
+}
