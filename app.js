00ce8d3402ed84c76cb9c56d943a35e2f9e432c4
Aldrin <aldrinmcaalim@gmail.com>
2021-03-24 17:12:15 -0400

    Created function that grabs max number in array

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
+// "use strict";
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
 
-const calcTip = bill => {
-    return bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
-}
+// const calcTip = (bill) => {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
 
-for (let i = 0; i < bills.length; i++) {
-    const tip = calcTip(bills[i]);
-    tips.push(tip);
-    totals.push(tip + bills[i]);
-}
+// for (let i = 0; i < bills.length; i++) {
+//   const tip = calcTip(bills[i]);
+//   tips.push(tip);
+//   totals.push(tip + bills[i]);
+// }
 
-// console.log(calcTip(bills));
+// // console.log(calcTip(bills));
 
-console.log(bills, tips, totals);
+// console.log(bills, tips, totals);
 
-// const numbers = [100, 100, 100, 100];
+// // const numbers = [100, 100, 100, 100];
 
-const calcAverage = array => {
-    let total = 0;
-    for (let i = 0; i < array.length; i++) {
-        total += array[i];
-    }
-    return total / array.length;
-}
+// const calcAverage = (array) => {
+//   let total = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     total += array[i];
+//   }
+//   return total / array.length;
+// };
 
-console.log(calcAverage(bills), calcAverage(tips), calcAverage(totals));
+// console.log(calcAverage(bills), calcAverage(tips), calcAverage(totals));
 
 // let averageBills = calcAverage(bills).toFixed(2);
 // averageBills = Number(averageBills);
@@ -37,3 +38,20 @@ console.log(calcAverage(bills), calcAverage(tips), calcAverage(totals));
 // let averageTotal = calcAverage(totals).toFixed(2);
 // averageTotal = Number(averageTotal);
 // console.log(averageTotal);
+
+const temperatures = [100, -3, -6, 34, "error", 16, -10, -20, 30, 234];
+
+const maxNumber = function (array) {
+  let largestNumber = array[0];
+  let i = 0;
+  for (i; i < array.length; i++) {
+    if (!Number(array[i])) {
+      continue;
+    } else if (array[i] > largestNumber) {
+      largestNumber = array[i];
+    }
+  }
+  return largestNumber;
+};
+
+console.log(maxNumber(temperatures));
