b2ba43d94aaeb2b45a8af92afa62f177c698212d
Aldrin <aldrinmcaalim@gmail.com>
2021-03-24 00:45:52 -0400

    Working

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
-const numbers = [100, 100, 100, 100];
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = bill => {
+    let tip;
+    let total;
+    for (let i = 0; i < bill.length; i++) {
+        if (bill[i] >= 50 && bill[i] <= 300) {
+            tip = bill[i] * .15;
+            tip = Number(tip.toFixed(2));
+            tips.push(tip);
+            total = bill[i] + tip;
+            total = Number(total.toFixed(2));
+            totals.push(total);
+        } else {
+            tip = bill[i] * .15;
+            tip = Number(tip.toFixed(2));
+            tips.push(tip);
+            total = bill[i] + tip;
+            total = Number(total.toFixed(2));
+            totals.push(total);
+        }
+    }
+}
+
+console.log(calcTip(bills));
+
+console.log(tips);
+console.log(totals);
+
+// const numbers = [100, 100, 100, 100];
 
 const calcAverage = array => {
     let total = 0;
@@ -8,4 +39,12 @@ const calcAverage = array => {
     return total / array.length;
 }
 
-console.log(calcAverage(numbers));
+let averageBills = calcAverage(bills).toFixed(2);
+averageBills = Number(averageBills);
+console.log(averageBills);
+let averageTips = calcAverage(tips).toFixed(2);
+averageTips = Number(averageTips);
+console.log(averageTips);
+let averageTotal = calcAverage(totals).toFixed(2);
+averageTotal = Number(averageTotal);
+console.log(averageTotal);
